3455b1cb015d7a7d8cd02b0879cd30bccab03143
ermiragashi14 <155022915+ermiragashi14@users.noreply.github.com>
2025-03-05 21:35:03 +0100

    Database Creation and Connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.hci_company_workspace_management_system {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.hci_company_workspace_management_system to javafx.fxml;
     exports com.example.hci_company_workspace_management_system;

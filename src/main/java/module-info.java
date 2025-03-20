197b4e3f66585ae95602f17a770bdfc9bdb41652
ermiragashi14 <155022915+ermiragashi14@users.noreply.github.com>
2025-03-20 03:47:23 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,16 +9,8 @@ module com.example.hci_company_workspace_management_system {
     requires mysql.connector.j;
     requires jakarta.mail;
 
-    // ✅ FIX: Allow JavaFX to access `RegisterController`
     opens controller to javafx.fxml;
 
-    // ✅ Ensure main package is accessible
-<<<<<<< HEAD
-=======
-    opens views to javafx.fxml, javafx.graphics;
->>>>>>> 0c1f48a318ff05ce132cdd7eb18c4fa671cdc449
-
-    // ✅ Export required packages
     exports controller;
     exports model;
     exports repository;
